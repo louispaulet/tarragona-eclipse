aefdfb73e9e67699d15374ec3a7a7ee45ebb9b17
Louis Paulet <louispaulet@macbook-air-de-louis.home>
2026-08-08 02:01:34 +0200

    Updates

---
+++ _next/static/chunks/04y4uq8~bjqe3.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,7073,t=>{"use strict";var e=t.i(43476),i=t.i(76983);t.s(["default",0,function({children:t}){return(0,e.jsx)(i.HashRouter,{children:t})}])},67868,t=>{t.n(t.i(7073))}]);
